cb45fd932c9171bfded2dc24215630172c74607e
eddd98 <eddd98@hotmail.com>
2022-03-11 13:40:02 -0600

    appjs fix

--- nodeApi/app.js
+++ nodeApi/app.js
@@ -5,9 +5,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const PORT = process.env.PORT || 5000;
-const URI = "mongodb+srv://eddd98:eddd1998@cluster0.hotyr.mongodb.net/ListaDB?retryWrites=true&w=majority"
+const URI = "mongodb+srv://eddd98:eddd1998@cluster0.hotyr.mongodb.net/ListaDB?retryWrites=true&w=majority";
 
-const Lista = require('./models/Articulo');
 const Articulo = require('./models/Articulo');
 const { collection, db } = require('./models/Articulo');
 const app = express();
@@ -19,16 +18,16 @@ app.use(express.json());
 app.listen(PORT, ()=>console.log("Listening in port: " + PORT));
 
 app.get("/", async(request, result)=>{
-    result.send("Hola")
+    result.send("Hola");
 })
 
 app.get("/lista", async(request, result)=>{
     try {
         const articulo = await Articulo.find();
-        result.json(articulo)
-        console.log("Datos leidos")
+        result.json(articulo);
+        console.log("Datos leidos");
     } catch (error) {
-        result.send({message:"Error cargando los datos"})
+        result.send({message:"Error cargando los datos"});
     }
 })
 
@@ -36,27 +35,43 @@ app.post("/lista", async(request, result) => {
     var articulo = new Articulo ({
         name: request.body.name,
         isChecked: request.body.isChecked 
-     })
-     await articulo.save()
-     result.send(JSON.stringify(articulo));
-     console.log("Dato Agregado")
+     });
+     try {
+        await articulo.save();
+        result.send(JSON.stringify(articulo));
+        console.log("Dato Agregado");
+     } catch (error) {
+        result.send({message:"Error enviando los datos"});
+     }
 })
 
 app.put("/lista/:id", async(request, result) => {
-    await Articulo.findByIdAndUpdate(request.params.id, { isChecked: true });
-    result.send({message:"Dato Eliminado"})
-    console.log("Dato actualizado")
+    try {
+        await Articulo.findByIdAndUpdate(request.params.id, { isChecked: true });
+        result.send({message:"Dato Eliminado"});
+        console.log("Dato actualizado");
+    } catch (error) {
+        result.send({message:"Error alterando los datos"});
+    }
 })
 
 app.delete("/lista/:id", async(request, result) => {
-    await Articulo.findByIdAndRemove(request.params.id);
-    result.send({message:"Dato Eliminado"})
-    console.log("Dato Eliminado")
+    try {
+        await Articulo.findByIdAndRemove(request.params.id);
+        result.send({message:"Dato Eliminado"});
+        console.log("Dato Eliminado");
+    } catch (error) {
+        result.send({message:"Error conenctandose a la DB"});
+    }
 })
 
 const connectDB = async()=>{
-    await mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true} );
-    console.log("DB conectada")
+    try {
+        await mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true} );
+        console.log("DB conectada");
+    } catch (error) {
+        result.send({message:"Error conenctandose a la DB"});
+    }
 }
 
 connectDB();
